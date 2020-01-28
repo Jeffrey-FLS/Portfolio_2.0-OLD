a1007df94f5c622df33bb0d87ad0b434bee0f868
Jeff <JP7Kode@gmail.com>
2020-01-27 23:59:17 -0500

    Added hambergur animate, propTypes, style-components, and currently working on react hooks for header, then carrying on to home component
    completion: pending       version: 0.1.0
    branch_type: foundation   version_type: minor
    branch: foundation_v0.1.0--creating_home-page

--- src/app/components/header/Header.component.jsx
+++ src/app/components/header/Header.component.jsx
@@ -1,17 +1,44 @@
 // React Components
-import React from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 
 // Assets
 import './Header.component.scss';
-import {Helmet} from "react-helmet";
+// import {Helmet} from "react-helmet";
+import {HamburgerSlider} from 'react-animated-burgers';
+import logo from './../../../assets/images/logo/logo_full.png';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
+
+    const prevCountRef = useRef();
+
+    const [isActive, setIsActive] = useState(false);
+
+    // useEffect(() => {
+    //     document.title = `You clicked ${count} times`;
+    // });
+
+        useEffect(() => {
+            function toggleButton() {
+                setIsActive()
+            }
+        });
+
     return (
         <header>
-            {/*<Helmet>*/}
-            {/*    <style>{'body { background-color: #212121; }'}</style>*/}
-            {/*</Helmet>*/}
-            <h1>Hello From Header</h1>
+            <div className="header_-_logo">
+                <Link to="/home"><img src={logo} alt="logo"/></Link>
+            </div>
+
+            <div className="header_-_menu">
+                {/*<HamburgerSlider*/}
+                {/*    className="header_-_nav-links_--_menu--hamburger"*/}
+                {/*    isActive={this.state.isActive}*/}
+                {/*    toggleButton={this.toggleButton}*/}
+                {/*    barColor="#fff"*/}
+                {/*    buttonWidth={30}*/}
+                {/*/>*/}
+            </div>
         </header>
     );
 };
