350203c6ffaa65b4069bb1f1175503ad000b5051
Jeff <JP7Kode@gmail.com>
2020-01-28 12:50:28 -0500

    Doing the last touches on header component and installed Material UI. Will be continuing on homePage afterwards
    completion: pending       version: 0.1.0
    branch_type: foundation   version_type: minor
    branch: foundation_v0.1.0--creating_home-page

--- src/app/components/header/Header.component.jsx
+++ src/app/components/header/Header.component.jsx
@@ -10,34 +10,26 @@ import {Link} from 'react-router-dom';
 
 const Header = () => {
 
-    const prevCountRef = useRef();
-
     const [isActive, setIsActive] = useState(false);
 
-    // useEffect(() => {
-    //     document.title = `You clicked ${count} times`;
-    // });
-
-        useEffect(() => {
-            function toggleButton() {
-                setIsActive()
-            }
-        });
+    const toggleButton = () => {
+        setIsActive(!isActive);
+    };
 
     return (
-        <header>
+        <header className="header">
             <div className="header_-_logo">
                 <Link to="/home"><img src={logo} alt="logo"/></Link>
             </div>
 
             <div className="header_-_menu">
-                {/*<HamburgerSlider*/}
-                {/*    className="header_-_nav-links_--_menu--hamburger"*/}
-                {/*    isActive={this.state.isActive}*/}
-                {/*    toggleButton={this.toggleButton}*/}
-                {/*    barColor="#fff"*/}
-                {/*    buttonWidth={30}*/}
-                {/*/>*/}
+                <HamburgerSlider
+                    className="header_-_menu--hamburger"
+                    isActive={isActive}
+                    toggleButton={toggleButton}
+                    barColor="#fff"
+                    buttonWidth={30}
+                />
             </div>
         </header>
     );
