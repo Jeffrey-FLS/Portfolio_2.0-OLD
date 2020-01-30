2840be7b41d9a8cd67e18f11e46859fbafd62429
Jeff <JP7Kode@gmail.com>
2020-01-30 18:59:21 -0500

    Social links animations are set, integrating the links
    completion: pending       version: 0.3.0
    branch_type: foundation   version_type: minor
    branch: foundation_v0.3.0--integrating_social_links

--- src/app/pages/home/components/sociaLinks/SocialLinks.component.jsx
+++ src/app/pages/home/components/sociaLinks/SocialLinks.component.jsx
@@ -14,15 +14,30 @@ import {
 
 const SocialLinks = () => {
     return (
-        <div className="social-links">
-            <div className="social-links_-_links">
-                <FaLinkedinIn className="social-links_-_links--link"/>
-                <FaCodepen className="social-links_-_links--link"/>
-                <FaGithubAlt className="social-links_-_links--link"/>
-                <FaFacebookF className="social-links_-_links--link"/>
+        <div className="social">
+            <div className="social_-_links">
+                <div className="social_-_links_--_link">
+                    <FaLinkedinIn className="social_-_links_--_link--icon"/>
+                    <h6>LinkedIn</h6>
+                </div>
+
+                <div className="social_-_links_--_link">
+                    <FaCodepen className="social_-_links_--_link--icon"/>
+                    <h6>Codepen</h6>
+                </div>
+
+                <div className="social_-_links_--_link">
+                    <FaGithubAlt className="social_-_links_--_link--icon"/>
+                    <h6>Github</h6>
+                </div>
+
+                <div className="social_-_links_--_link">
+                    <FaFacebookF className="social_-_links_--_link--icon"/>
+                    <h6>Facebook</h6>
+                </div>
             </div>
 
-            <div className="social-links_-_title">
+            <div className="social_-_title">
                 <h6>Find me on</h6>
             </div>
         </div>
