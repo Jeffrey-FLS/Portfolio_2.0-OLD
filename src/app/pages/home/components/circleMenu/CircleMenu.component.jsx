611f25755fbefbbdfaa8eadf200a84c1e39af286
Jeff <JP7Kode@gmail.com>
2020-01-31 19:26:10 -0500

    pushing to github

--- src/app/pages/home/components/circleMenu/CircleMenu.component.jsx
+++ src/app/pages/home/components/circleMenu/CircleMenu.component.jsx
@@ -6,18 +6,16 @@ import './CircleMenu.component.scss';
 
 const CircleMenu = () => {
     return (
-        <div className="circle-menu">
+        // <div className="circle-menu">
             <div className="circle">
-
-                <div className="outer-side">
-                    <div className="circle-side side-1"></div>
-                </div>
-                <div className="circle-side side-2"></div>
-                <div className="circle-side side-3"></div>
-                <div className="circle-side side-4"></div>
-                <div className="circle-side side-5"></div>
+                <div className="circle-side side-1"/>
+                <div className="circle-side side-2"/>
+                <div className="circle-side side-3"/>
+                <div className="circle-side side-4"/>
+                <div className="circle-side side-5"/>
+                <div className="circle-center"/>
             </div>
-        </div>
+        // </div>
     );
 };
 
