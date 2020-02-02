c56ff59ef0ab82bffcd263b397f6c1903426a985
Jeff <JP7Kode@gmail.com>
2020-02-02 17:10:42 -0500

    Importing react scrollbar

--- src/app/pages/home/HomePage.component.jsx
+++ src/app/pages/home/HomePage.component.jsx
@@ -10,22 +10,31 @@ import {SocialLinks, CircleMenu} from './components';
 
 const HomePage = () => {
     return (
-        // <div className="home-base">
-            <div className="home">
+        <div className="home container-fluid">
 
-            {/*<Helmet>*/}
-            {/*    <style>{'body { background-color: #212121; }'}</style>*/}
-            {/*</Helmet>*/}
+            <Helmet>
+                <style>{'body { background-color: #212121; }'}</style>
+            </Helmet>
+
+            <div className="container">
+                <div className="row">
+                    <div className="home_-_intro-about offset-1 col-10 offset-1">
+                        <h3>Hi, my name is </h3>
+                        <h1>Jeffrey Paulino</h1>
+                        <h1>I build things for the web</h1>
+                    </div>
+                </div>
+            </div>
+
 
             {/*<div className="home_-_circle-menu">*/}
             {/*    <CircleMenu/>*/}
             {/*</div>*/}
 
-            {/*<div className="home_-_social-links">*/}
-            {/*    <SocialLinks/>*/}
-            {/*</div>*/}
+            <div className="home_-_social-links">
+                <SocialLinks/>
             </div>
-         // </div>
+        </div>
     );
 };
 
