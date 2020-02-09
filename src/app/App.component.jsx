6c7871d44f6f833a7e210ec648a20727af86908a
Jeff <JP7Kode@gmail.com>
2020-02-08 19:47:28 -0500

    Created Sections which will be the content of the site
    branch_status: pending    version: 0.5.0
    branch_type: foundation   version_type: minor
    branch: foundation_v0.5.0--homepage_design_change

--- src/app/App.component.jsx
+++ src/app/App.component.jsx
@@ -20,10 +20,18 @@ function App() {
       <div className="App">
           <Header/>
 
-          <Switch>
-          {/*<Route path="/userMain" render={(routerProps) => <UserMenuPage quizzes={this.state.quizzes} {...routerProps}/>}/>*/}
-          <Route path="/" component={HomePage}/>
-        </Switch>
+          <div className="app_-_home">
+              <HomePage/>
+          </div>
+
+          <main className="app_-_main container">
+
+          </main>
+
+        {/*  <Switch>*/}
+        {/*  /!*<Route path="/userMain" render={(routerProps) => <UserMenuPage quizzes={this.state.quizzes} {...routerProps}/>}/>*!/*/}
+        {/*  <Route path="/" component={HomePage}/>*/}
+        {/*</Switch>*/}
 
 
           <div className="app_-_footer container">
---
+++ src/app/components/about/template.component.jsx
@@ -0,0 +1,13 @@
+// React Components
+import React from 'react';
+
+// Assets
+import './AboutSection.component.scss';
+
+const AboutSection = () => {
+    return (
+        <h1>Hello From AboutSection</h1>
+    );
+};
+
+export default AboutSection;
