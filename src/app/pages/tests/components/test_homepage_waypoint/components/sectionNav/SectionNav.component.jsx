d8f81d9d1abc00831320ea94513b0629872004df
Jeff <JP7Kode@gmail.com>
2020-03-23 15:25:55 -0400

    Still working on SectionNav
    branch_status: pending    version: 0.6.1
    branch_type: foundation   version_type: minor
    branch: foundation_v0.6.1--react_waypoint_animation

--- src/app/pages/tests/components/test_homepage_waypoint/components/sectionNav/SectionNav.component.jsx
+++ src/app/pages/tests/components/test_homepage_waypoint/components/sectionNav/SectionNav.component.jsx
@@ -1,5 +1,5 @@
 // React Components
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 // Assets
@@ -7,7 +7,8 @@ import "./SectionNav.component.scss";
 
 const SectionNav = props => {
   // const { numOfSections, listOfSections } = props;
-  const [sections, setSections] = useState(null);
+  const [sections, setSections] = useState();
+  const [currentSection, setCurrentSection] = useState(null);
 
   const numOfSections = 6;
   const listOfSections = null;
@@ -16,35 +17,82 @@ const SectionNav = props => {
   //   return <div className="section-nav_-_scroll_--_line" />;
   // };
 
-  const populateSections = () => {
-      
-  };
+  // const populateSections = () => {
 
-  const renderSections = (intSections, arrStrSections) => {
-    const className = "line_";
-    let objSections = {};
+  // };
 
-    if (intSections) {
-      for (let i = 0; i < intSections; i++) {
-        objSections[className + i] = {
-          index: i,
-          name: className + i
-        }
-      }
-    } else if (arrStrSections) {
-      objSections = arrStrSections.map(strSection => (
-        <div className={className}>
-          <h3>{strSection}</h3>
-        </div>
-      ));
-    } else {
-      console.warn('intSections or arrStrSections value does not exist');
+  const convertNumOfSectionsList = num => {
+    let arr = new Array(num);
+
+    for (let i = 0; i < num; i++) {
+      arr[i] = "section_" + i;
     }
 
-    // alert(JSON.stringify(objSections));
-
-    return objSections;
+    return arr;
   };
+
+  const populateSections = arrStrSections => {
+    let objSections = { section: new Array(arrStrSections.length) };
+
+    arrStrSections.forEach(
+      (strSection, index) =>
+        (objSections.section[index] = {
+          index: index,
+          name: strSection,
+          element: (
+            <div className={"line_" + strSection}>
+              <h3>{strSection}</h3>
+            </div>
+          )
+        })
+    );
+
+    setSections(objSections);
+  };
+
+  useEffect(() => {
+    const sectionList = numOfSections
+      ? convertNumOfSectionsList(numOfSections)
+      : listOfSections;
+
+    populateSections(sectionList);
+
+    alert(JSON.stringify(sections));
+  }, []);
+
+  // const renderSections = (intSections, arrStrSections) => {
+  //   const className = "line_";
+  //   let objSections = { section: [] };
+
+  //   if (intSections) {
+  //     for (let i = 0; i < intSections; i++) {
+  //       objSections.section.push({
+  //         index: i,
+  //         name: className + i,
+  //         elem: <div className={className}/>
+  //       });
+  //     }
+  //   } else if (arrStrSections) {
+  //     objSections = arrStrSections.map((strSection, index) =>
+  //       objSections.section.push({
+  //         index: index,
+  //         name: strSection,
+  //         elem: (
+  //           <div className={className}>
+  //             <h3>{strSection}</h3>
+  //           </div>
+  //         )
+  //       })
+  //     );
+  //   } else {
+  //     console.warn("intSections or arrStrSections value does not exist");
+  //   }
+
+  //   alert(JSON.stringify(objSections));
+
+  //   // return objSections;
+  //   return <h1></h1>;
+  // };
 
   // const renderSections = (intSections, arrStrSections) => {
   //   const className = "section-nav_--_scroll_---_line";
@@ -67,26 +115,27 @@ const SectionNav = props => {
   //   return arrDivSections;
   // };
 
-  // const sectionList = num => {
-  //   let arr = new Array(num);
+  // const nextSection = () => {
+  //   const newIndex = this.state.property.index+1;
+  //   this.setState({
+  //     property: data.properties[newIndex]
+  //   })
+  // }
 
-  //   for (let i = 0; i < num; i++) {
-  //     arr[i] = `Section-${i + 1}`;
-  //   }
+  // const prevSection = () => {
+  //   const newIndex = this.state.property.index-1;
+  //   this.setState({
+  //     property: data.properties[newIndex]
+  //   })
+  // }
 
-  //   return arr;
-  // };
+  // renderSections(numOfSections, listOfSections);
 
   return (
     <nav className="section-nav">
       <div className="section-nav_-_active">
         <div className="section-nav_-_active_--_scroll">
-          {/* {renderSections(numOfSections, listOfSections)} */}
-
-          {
-            {/* for */}
-          }
-
+          {/* {sections.section.map(sec => sec.element)} */}
         </div>
       </div>
     </nav>
