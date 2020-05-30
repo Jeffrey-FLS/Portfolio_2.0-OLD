5a6d5ad685d318f590b1eecd582af56055df61dd
Jeff <JP7Kode@gmail.com>
2020-05-30 11:59:57 -0400

    Delected 'react-gsap' from pakage.json, commented imports that call in react-gsap. Working on fixing console errors
    branch_status: pending    version: 0.6.1
    branch_type: foundation   version_type: minor
    branch: foundation_v0.6.1--react_waypoint_animation

--- src/app/pages/tests/components/index.js
+++ src/app/pages/tests/components/index.js
@@ -1,13 +1,13 @@
 
 import TestCarousel from './test_carousel/TestCarousel.component';
 import TestSMClassToggle from './test_scrollmagic_class_toggle/TestSMClassToggle.component';
-import TestSMComponents from './test_scrollmagic_components/TestSMComponents.component';
+// import TestSMComponents from './test_scrollmagic_components/TestSMComponents.component';
 import TestHomePage from './test_homepage/TestHomePage.component';
 import TestRef from './test_ref/TestRef.component';
 import TestOnScroll from './test_onscroll/TestOnScroll.component';
 import TestWaypoint from './test_waypoint/TestWaypoint.component';
-import TestWaypointComponents from './test_waypoint-components/TestWaypointComponents.component';
-import TestWaypointComponents2 from './test_waypoint-components2/TestWaypointComponents2.component';
+// import TestWaypointComponents from './test_waypoint-components/TestWaypointComponents.component';
+// import TestWaypointComponents2 from './test_waypoint-components2/TestWaypointComponents2.component';
 import TestWaypointBg from './test_waypoint-bg/TestWaypointBg.component';
 import TestHomepageWaypoint from './test_homepage_waypoint/TestHomepageWaypoint.component';
 
@@ -15,13 +15,13 @@ import TestHomepageWaypoint from './test_homepage_waypoint/TestHomepageWaypoint.
 const TestComponents = {
     TestCarousel,
     TestSMClassToggle,
-    TestSMComponents,
+    // TestSMComponents,
     TestHomePage,
     TestRef,
     TestOnScroll,
     TestWaypoint,
-    TestWaypointComponents,
-    TestWaypointComponents2,
+    // TestWaypointComponents,
+    // TestWaypointComponents2,
     TestWaypointBg,
     TestHomepageWaypoint
 };
