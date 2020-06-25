9776826bfcfdd16ce0df3dec689609926fdad9d3
Jeffrey Paulino <jp7kode@gmail.com>
2020-06-25 17:22:18 -0400

    Rerouting progressbar location
    branch_status: pending    version: 0.7.1
    branch_type: foundation   version_type: minor
    branch: foundation_v0.7.1--project_section

---
+++ src/app/components/sections/projects/components/carousel/components/control/components/progress/components/index.js
@@ -0,0 +1,7 @@
+import ProgressBar from './progress_bar/ProgressBar.component';
+import TestProgressBar from './tests/test_progress_bar/TestProgressBar.component';
+
+export {
+    ProgressBar,
+    TestProgressBar
+}
