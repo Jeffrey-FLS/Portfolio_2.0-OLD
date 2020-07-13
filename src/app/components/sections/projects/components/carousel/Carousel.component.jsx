b7f764a57d39b44a385e056a64f42342ae33df39
Jeffrey Paulino <jp7kode@gmail.com>
2020-07-12 21:55:30 -0400

    commitinh

--- src/app/components/sections/projects/components/carousel/Carousel.component.jsx
+++ src/app/components/sections/projects/components/carousel/Carousel.component.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 // Assets
 import "./Carousel.component.scss";
 
-// Committing 
+// Committing to push
 
 // import images from "assets/images";
 import images from "assets/images";
